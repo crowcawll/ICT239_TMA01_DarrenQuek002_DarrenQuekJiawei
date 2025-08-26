ad506199f1e98c0136cc9080e9fb8c405904c902
crowcawll <darrenquek002@suss.edu.sg>
2025-08-26 14:36:09 +0800

    fix: Align main content to right by updating code to Bootstrap 5.

--- 2a/app/app.py
+++ 2a/app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, blueprints
 app = Flask(__name__)
 
+# Register Blueprint so we can factor routes
+from controllers.bookTitleController import bookTitle
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+# register blueprint from respective module
+app.register_blueprint(bookTitle)
 
 @app.route('/base')
 def show_base():
